47b54afc0470001ae8b4946d76a2e10431847a98
Marie-Charlotte LEVITRE <destroyadenyum@gmail.com>
2023-09-21 15:38:02 +0200

    Creation: Front and Back folders, Header and Footer components and Home page

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import Logo from '../assets/images/argentBankLogo.png';
+
+function Header() {
+    return (
+        <header>
+            <nav class="main-nav">
+                <NavLink class="main-nav-logo" to={'/'}>
+                    <img class="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
+                    <h1 class="sr-only">Argent Bank</h1>
+                </NavLink>
+                <div>
+                    <NavLink class="main-nav-item" href="./sign-in.html">
+                        <i class="fa fa-user-circle"></i>
+                        Sign In
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
---
+++ front/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+import App from './App';
+import Home from './pages/Home';
+
+import './style/main.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ]
+  },
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
